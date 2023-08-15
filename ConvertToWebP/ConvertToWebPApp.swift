e870e4f89dfc5a6d7f8695ba4f81042c464e3b91
mrzeappleGit <mrzeapple42@gmail.com>
2023-08-15 00:13:20 -0500

    Initial Commit

---
+++ ConvertToWebP/ConvertToWebPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertToWebPApp.swift
+//  ConvertToWebP
+//
+//  Created by Matthew Stevens on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ConvertToWebPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
